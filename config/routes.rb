86d1d7f8bd909484a8b56e014872c3e913e7ed4d
Josh <josh@yoomee.com>
2019-05-10 14:09:30 +0100

    added user accounts to routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
 
   # TODO: limit onbly to used actions!
   resources :sites
-  post '/sites/new', to: 'sites#create'
   resources :students
+  resources :user_accounts
+  post '/sites/new', to: 'sites#create'
   post '/students/new', to: 'students#create'
+  post '/user_accounts/new', to: 'user_accounts#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
