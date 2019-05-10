396184a549aeb6b3dba5bc2058014a12797bb913
Josh <josh@yoomee.com>
2019-05-10 11:02:54 +0100

    added creating and editing actions

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -6,4 +6,45 @@ class SitesController < ApplicationController
   def show
     @site = Site.find_by_id(params[:id])
   end
+
+  def edit
+    @site = Site.find_by_id(params[:id])
+  end
+
+  def update
+    @site = Site.find(params[:id])
+    if @site.update_attributes(site_params)
+      flash[:notice] = 'Site updated successfully'
+      redirect_to site_path(@site)
+    else
+      flash[:alert] = "Error: #{@site.errors.full_messages.to_sentence}"
+      render 'edit'
+    end
+  end
+
+  def new
+    @site = Site.new
+    binding.pry
+  end
+
+  def create
+    @site = Site.new(site_params)
+    if @site.save
+      flash[:notice] = 'Site added!'
+      redirect_to site_path(@site)
+    else
+      flash[:alert] = "Error: #{@site.errors.full_messages.to_sentence}"
+      render 'new'
+    end
+  end
+
+  def site_params
+    params.permit(site: [
+                        :name,
+                        :capacity,
+                        :region,
+                        :manager,
+                        :notes
+                        ])[:site]
+  end
 end
