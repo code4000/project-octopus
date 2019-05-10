92aa38ee3cf69706ecc79bd7c635b82435155df1
Josh <josh@yoomee.com>
2019-05-10 10:21:07 +0100

    created age and name methods in student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,14 @@
 class Student < ApplicationRecord
   belongs_to :site, optional: true
   acts_as_taggable_on :tags
+
+  def age
+    dob = self.dob
+    now = Time.now.utc.to_date
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
+
+  def name
+    self.first_name + ' ' + self.last_name
+  end
 end
