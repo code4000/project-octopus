04963a445876d11e4fd8830c1294031817822c88
Josh <josh@yoomee.com>
2019-05-10 11:41:23 +0100

    update and create students actions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,4 +6,50 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find_by_id(params[:id])
   end
+
+  def edit
+    @student = Student.find_by_id(params[:id])
+  end
+
+  def update
+    @student = Student.find_by_id(params[:id])
+    if @student.update_attributes(student_params)
+      flash[:notice] = 'Student updated successfully'
+      redirect_to student_path(@student)
+    else
+      flash[:alert] = "Error: #{@student.errors.full_messages.to_sentence}"
+      render 'edit'
+    end
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:notice] = 'Student added!'
+      redirect_to student_path(@student)
+    else
+      flash[:alert] = "Error: #{@student.errors.full_messages.to_sentence}"
+      render 'new'
+    end
+  end
+
+  def student_params
+    params.permit(student: [
+                        :first_name,
+                        :last_name,
+                        :prison_number,
+                        :site_id,
+                        :gender,
+                        :dob,
+                        :crd,
+                        :hdc,
+                        :rotl,
+                        :recat,
+                        :notes
+                        ])[:student]
+  end
 end
