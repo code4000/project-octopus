3bbc2afac7fbdfbe850afc097efd025346128f36
Josh <josh@yoomee.com>
2019-05-10 14:20:55 +0100

    set up user account methods

--- app/controllers/user_accounts_controller.rb
+++ app/controllers/user_accounts_controller.rb
@@ -1,6 +1,48 @@
-class UsersController < ApplicationController
+class UserAccountsController < ApplicationController
   load_and_authorize_resource class: User, except: [:create]
   def index
     @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:notice] = 'User updated successfully'
+      redirect_to user_accounts_path
+    else
+      flash[:alert] = "Error: #{@admin.errors.full_messages.to_sentence}"
+      render 'edit'
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    require 'securerandom'
+    @user = User.new(user_params)
+    @user.password = SecureRandom.hex(8)
+    if @user.save
+      # TODO: email user password
+      # AdminMailer.with(admin: @admin).welcome_email.deliver_now
+      flash[:notice] = 'Account added!'
+      redirect_to user_accounts_path
+    else
+      flash[:alert] = "Error: #{@user.errors.full_messages.to_sentence}"
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.permit(user: [
+                        :name,
+                        :email,
+                        :role,
+                        ])[:user]
   end
 end
