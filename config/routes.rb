6eb546a2fd24d5bd5b9bb0d4b925d141d1b99ef1
Josh <josh@yoomee.com>
2019-05-10 11:02:03 +0100

    post new sites and students to create actions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   # TODO: limit onbly to used actions!
   resources :sites
+  post '/sites/new', to: 'sites#create'
   resources :students
+  post '/students/new', to: 'students#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
