c595983cc5c886229c8be9ee42c9ea5e43599c04
Josh <josh@yoomee.com>
2019-05-14 12:59:58 +0100

    add post new tag entries to controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,18 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   # TODO: limit onbly to used actions!
-  resources :sites
-  resources :students
+  resources :sites do
+    post :add_regions, on: :collection
+    post :add_tags, on: :collection
+  end
+  resources :students do
+    post :add_skills, on: :collection
+    post :add_job_preferences, on: :collection
+    post :add_tags, on: :collection
+  end
   resources :user_accounts
+  
   post '/sites/new', to: 'sites#create'
   post '/students/new', to: 'students#create'
   post '/user_accounts/new', to: 'user_accounts#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
