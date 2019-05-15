a63daef4a8896f929f52b9c0e858dd81679176cc
Josh <josh@yoomee.com>
2019-05-15 10:21:02 +0100

    validate presesence of name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates_presence_of :name
+
   def master?
     if self.role == "master"
       true
