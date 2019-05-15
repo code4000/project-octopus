dd8b2de1c6c61eae73fe4a80ea43089f83958a20
Josh <josh@yoomee.com>
2019-05-15 14:43:20 +0100

    sending correct reset password link when new user generated

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,11 @@
 class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
+    raw, enc = Devise.token_generator.generate(User, :reset_password_token)
+    @token = raw
+    @user.reset_password_token = enc
+    @user.reset_password_sent_at = Time.now.utc
+    @user.save(validate: false)
     mail(to: @user.email, subject: 'Welcome to Octopus')
   end
 end
