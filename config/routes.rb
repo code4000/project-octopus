d0a6acb332abc9db65ac4adbbfd09908f5d7922a
Josh <josh@yoomee.com>
2019-05-15 11:17:56 +0100

    updated routes to include searches

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,17 @@ Rails.application.routes.draw do
 
   # TODO: limit onbly to used actions!
   resources :sites do
-    post :add_regions, on: :collection
-    post :add_tags, on: :collection
+    collection do
+      get 'search'
+    end
   end
   resources :students do
-    post :add_skills, on: :collection
-    post :add_job_preferences, on: :collection
-    post :add_tags, on: :collection
+    collection do
+      get 'search'
+    end
   end
   resources :user_accounts
-  
+
   post '/sites/new', to: 'sites#create'
   post '/students/new', to: 'students#create'
   post '/user_accounts/new', to: 'user_accounts#create'
