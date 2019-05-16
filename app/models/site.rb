55e774922ff50113c3bd78c33a1154c5269c096f
Josh <josh@yoomee.com>
2019-05-16 09:59:10 +0100

    added hub? to sites

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,4 +3,12 @@ class Site < ApplicationRecord
   acts_as_taggable_on :regions, :tags
   validates_presence_of :name
   validates_uniqueness_of :name
+
+  def hub?
+    if self.tag_list.include?("Hub")
+      true
+    else
+      false
+    end
+  end
 end
