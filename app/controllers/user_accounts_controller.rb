b82f8aeb3ecdb64995048a36cdfc49278d2e1471
Josh <josh@yoomee.com>
2019-05-16 10:35:59 +0100

    order by name

--- app/controllers/user_accounts_controller.rb
+++ app/controllers/user_accounts_controller.rb
@@ -1,7 +1,7 @@
 class UserAccountsController < ApplicationController
   load_and_authorize_resource class: User, except: [:create]
   def index
-    @users = User.all
+    @users = User.all.order(:name)
   end
 
   def edit
