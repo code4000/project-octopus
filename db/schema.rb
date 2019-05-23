8a7648624222bf8b4aed60cd8323fa09d2c40ad9
Josh <josh@yoomee.com>
2019-05-23 14:08:31 +0100

    added contact column to contacts in db, this allows us to import from csv

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_111831) do
+ActiveRecord::Schema.define(version: 2019_05_23_122250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_05_22_111831) do
     t.text "about"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "contact"
   end
 
   create_table "sites", force: :cascade do |t|
