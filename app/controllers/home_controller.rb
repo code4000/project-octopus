29b1ae1718eafd32d2abc923c796d9f200d86d1a
Josh <josh@yoomee.com>
2019-05-23 13:01:00 +0100

    tidied home controller - more readable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,17 @@
 class HomeController < ApplicationController
   def index
-    if !current_user
-      redirect_to new_user_session_path
-    else
+    redirect_to new_user_session_path if !current_user
 
-      @all_sites = Site.all.order(:name).limit(5)
+    @all_sites = Site.all.order(:name).limit(5)
 
-      @most_popular_tags = {}
-      tags = Site.tag_counts_on(:regions).most_used(5)
-
-      tags.each do |tag|
-        @most_popular_tags[tag.name] = Site.tagged_with(tag.name).order(:name).limit(5)
-      end
-
-      activities = PublicActivity::Activity.all.order('created_at desc')
-      @activities = activities.paginate(page: params[:page], per_page: 20)
+    tags = Site.tag_counts_on(:regions).most_used(5)
+    @most_popular_tags = {}
+    tags.each do |tag|
+      @most_popular_tags[tag.name] = Site.tagged_with(tag.name).order(:name).limit(5)
     end
+
+    activities = PublicActivity::Activity.all.order('created_at desc')
+    @activities = activities.paginate(page: params[:page], per_page: 20)
+    
   end
 end
