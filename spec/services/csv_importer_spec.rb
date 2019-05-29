a96d2a9e2974e20162f9b37c4e99700d1c544883
Josh <josh@yoomee.com>
2019-05-29 12:20:55 +0100

    fixed errors with placement of shared examples - tests now run and all fail

--- spec/services/csv_importer_spec.rb
+++ spec/services/csv_importer_spec.rb
@@ -1,30 +1,4 @@
-import 'spec_helper'
-
-describe CSVImporter do
-  # TOOD: partial success
-  # TOOD: full success
-  # TODO: duplicates - warning/silent/update?
-
-  describe 'non-CSV file' do
-    it_behaves_like 'an invalid import'
-    # TODO: errors hash check message
-  end
-
-  describe 'headerless CSV file' do
-    it_behaves_like 'an invalid import'
-    # TODO: errors hash check message
-  end
-
-  describe 'CSV file with invalid headers' do
-    it_behaves_like 'an invalid import'
-    # TODO: errors hash check message
-  end
-
-  describe 'valid CSV with ' do
-    it_behaves_like 'an invalid import'
-    # TODO: errors hash check message
-  end
-end
+require 'rails_helper'
 
 shared_examples "an invalid import" do
   describe "#validate" do
@@ -73,5 +47,31 @@ shared_examples "an invalid import" do
     it 'is not empty' do
       expect(importer.errors).not_to be_empty
     end
+  end
+end
+
+describe CSVImporter do
+  # TOOD: partial success
+  # TOOD: full success
+  # TODO: duplicates - warning/silent/update?
+
+  describe 'non-CSV file' do
+    it_behaves_like 'an invalid import'
+    # TODO: errors hash check message
+  end
+
+  describe 'headerless CSV file' do
+    it_behaves_like 'an invalid import'
+    # TODO: errors hash check message
+  end
+
+  describe 'CSV file with invalid headers' do
+    it_behaves_like 'an invalid import'
+    # TODO: errors hash check message
+  end
+
+  describe 'valid CSV with ' do
+    it_behaves_like 'an invalid import'
+    # TODO: errors hash check message
   end
 end
