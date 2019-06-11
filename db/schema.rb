5d7e2ae55b901683456defa35f52ceb88b501268
Josh <josh@yoomee.com>
2019-06-11 12:50:16 +0100

    created otp secret key column in user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_111831) do
+ActiveRecord::Schema.define(version: 2019_06_11_112839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_05_22_111831) do
     t.text "about"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "contact"
   end
 
   create_table "sites", force: :cascade do |t|
@@ -121,6 +122,7 @@ ActiveRecord::Schema.define(version: 2019_05_22_111831) do
     t.datetime "updated_at", null: false
     t.string "role"
     t.string "name", null: false
+    t.string "otp_secret_key"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
