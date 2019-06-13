ef2e73cff391eaad5597d414a5766ff5c087ec78
Josh <josh@yoomee.com>
2019-06-13 11:31:47 +0100

    got rid of now unneeded csv import code

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,4 @@
 class Contact < ApplicationRecord
-  require 'csv'
   include PublicActivity::Model
   tracked owner: Proc.new{ |controller, model| controller.current_user }
 
@@ -10,13 +9,6 @@ class Contact < ApplicationRecord
 
   def name
     self.first_name + ' ' + self.last_name
-  end
-
-  def self.import(file)
-    CSV.foreach(file.path, headers: true) do |row|
-      # TODO: show error message if the next line fails (because attributes in file arent correct)
-      Contact.create(row.to_hash)
-    end
   end
 
 end
