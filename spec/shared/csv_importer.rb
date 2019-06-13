4aa44a09c3d70ee048ff51f6e7948e46b7be1612
Josh <josh@yoomee.com>
2019-06-13 14:37:16 +0100

    moved csv importer tests to a shared file so we can define each seperate import class as needing to behave like the shared file

---
+++ spec/shared/csv_importer.rb
@@ -0,0 +1,92 @@
+shared_examples "a csv importer" do
+  let!(:csv_directory) { "#{described_class.to_s.underscore}_csv_files" }
+
+  describe 'non-CSV file' do
+    let!(:subject) { described_class.new( Rails.root.join("spec", "services", csv_directory, "non_csv.txt") ) }
+    it_behaves_like 'an invalid record'
+    it_behaves_like 'a failed import'
+    # TODO: errors hash check message
+  end
+
+  describe 'headerless CSV file' do
+    let!(:subject) { described_class.new( Rails.root.join('spec', 'services', csv_directory, 'headerless.csv') ) }
+    it_behaves_like 'an invalid record'
+    it_behaves_like 'a failed import'
+    # TODO: errors hash check message
+  end
+
+  describe 'CSV file with invalid headers' do
+    let!(:subject) { described_class.new( Rails.root.join('spec', 'services', csv_directory, 'invalid_headers.csv') ) }
+    it_behaves_like 'an invalid record'
+    it_behaves_like 'a failed import'
+    # TODO: errors hash check message
+  end
+
+  describe 'valid CSV with failed records' do
+    let!(:subject) { described_class.new( Rails.root.join('spec', 'services', csv_directory, 'invalid_records.csv') ) }
+    # TODO: check has imported some stuff
+    # TODO: errors hash check message
+  end
+
+  describe 'valid CSV with duplicates' do
+    let!(:contact) { Contact.create(first_name: "Michael", last_name: "Angelo", email: "art@example.com") }
+    let!(:subject) { described_class.new( Rails.root.join('spec', 'services', csv_directory, 'valid_duplicates.csv') ) }
+    before {subject.save}
+
+    it 'has no errors' do
+      expect(subject.errors).to be_empty
+    end
+
+    it 'doesnt create new records' do
+      expect(Contact.count).to eq(1)
+    end
+
+    it 'has updated the duplicates' do
+      expect(Contact.first.email).to eq(contact.email)
+      expect(Contact.first.first_name).to eq(contact.first_name)
+      expect(Contact.first.last_name).to eq(contact.last_name)
+      expect(Contact.first.role).to eq("Artist")
+      expect(Contact.first.organisation).to eq("Vatican")
+    end
+  end
+
+  describe 'valid CSV import' do
+    let!(:subject) { described_class.new( Rails.root.join('spec', 'services', csv_directory, 'valid.csv') ) }
+
+    describe '#save' do
+      let!(:save_result) { subject.save }
+
+      it 'save returns true' do
+        expect(save_result).to be_truthy
+      end
+
+      it 'has no errors' do
+        expect(subject.errors).to be_empty
+      end
+
+      it 'creates new records' do
+        expect(Contact.count).to eq(3)
+      end
+    end
+
+    describe '#save!' do
+      let!(:save_result) { subject.save! }
+
+      it 'save returns true' do
+        expect(save_result).to be_truthy
+      end
+
+      it 'has no errors' do
+        expect(subject.errors).to be_empty
+      end
+
+      it 'creates new records' do
+        expect(Contact.count).to eq(3)
+      end
+      # returns true
+    end
+
+    # TODO check everything's good
+  end
+
+end
