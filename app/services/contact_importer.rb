03a7289d56ac4ba1a05305124116482d78b760e6
Josh <josh@yoomee.com>
2019-06-17 11:54:41 +0100

    refactored importers to take nearly all code out of the subclasses

--- app/services/contact_importer.rb
+++ app/services/contact_importer.rb
@@ -1,18 +1,7 @@
 class ContactImporter < CSVImporter
 
-  def import_class
-    self.class.import_class
-  end
-
-  def required_headers
-    [:first_name, :last_name, :email]
-  end
-
-  def headers_to_find_duplicates_by
-    [:email]
-  end
-
   def self.import_class
     Contact
   end
+
 end
--- app/services/student_importer.rb
+++ app/services/student_importer.rb
@@ -1,17 +1,5 @@
 class StudentImporter < CSVImporter
 
-  def import_class
-    self.class.import_class
-  end
-
-  def required_headers
-    [:first_name, :last_name, :email, :prison_number]
-  end
-
-  def headers_to_find_duplicates_by
-    [:email, :prison_number]
-  end
-
   def self.import_class
     Student
   end
