d2d912c9396b6a1b92736374ab0f307292e819de
Andy Mayer <andy@yoomee.com>
2019-07-05 10:04:01 +0100

    Fix seeds to work with strong passwords

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,7 @@
 
 puts ""
 puts "Creating seed data..."
-User.create(name: "Master User", email: "admin@yoomee.com", password: "logmein", role: "master")
+User.create(name: "Master User", email: "admin@yoomee.com", password: "10gm31n10gm31n", role: "master")
 puts ""
 puts "Created admin user <admin@yoomee.com> with password 'logmein'"
 puts ""
-
